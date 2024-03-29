d4096a262980ed5d2508588c1c65df56e66e8d92
David Kochkin <kochkin.david@gmail.com>
2024-03-29 18:27:21 +0200

    First Commit

---
+++ RickAndMorty/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by David Kochkin on 29.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
