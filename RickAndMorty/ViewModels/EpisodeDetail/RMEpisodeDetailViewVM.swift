9d5bf4bb0d379ea459a2d961b8c5de1327a06d09
David Kochkin <kochkin.david@gmail.com>
2024-04-22 10:32:42 +0300

    Set correct Date

--- RickAndMorty/ViewModels/EpisodeDetail/RMEpisodeDetailViewVM.swift
+++ RickAndMorty/ViewModels/EpisodeDetail/RMEpisodeDetailViewVM.swift
@@ -43,12 +43,18 @@ final class RMEpisodeDetailViewVM {
         
         let episode    = dataTuple.episode
         let characters = dataTuple.characters
+        
+        var createdString = episode.created
+        if let date       = RMCharacterInfoCollectionViewCellVM.dateFormatter.date(from: episode.created) {
+            createdString = RMCharacterInfoCollectionViewCellVM.shortDateFormatter.string(from: date)
+        }
+        
         cellViewModels = [
             .information(viewModel: [
                 .init(title: "Episode Name", value: episode.name),
                 .init(title: "Air Date",     value: episode.air_date),
                 .init(title: "Episode",      value: episode.episode),
-                .init(title: "Created",      value: episode.created),
+                .init(title: "Created",      value: createdString),
             ]),
             .characters(
                 viewModel: characters.compactMap({ character in
