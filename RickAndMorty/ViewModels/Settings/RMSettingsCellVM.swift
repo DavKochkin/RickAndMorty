d640b96fe0e78f19090ce734454d5e8e89a1869e
David Kochkin <kochkin.david@gmail.com>
2024-04-23 11:04:44 +0300

    Add SettingsOption

---
+++ RickAndMorty/ViewModels/Settings/RMSettingsCellVM.swift
@@ -0,0 +1,14 @@
+//
+//  RMSettingsCellVM.swift
+//  RickAndMorty
+//
+//  Created by David Kochkin on 23.04.2024.
+//
+
+import UIKit
+
+
+struct RMSettingsCellVM {
+    let image: UIImage?
+    let title: String? 
+}
---
+++ RickAndMorty/ViewModels/Settings/RMSettingsViewVM.swift
@@ -0,0 +1,17 @@
+//
+//  RMSettingsViewVM.swift
+//  RickAndMorty
+//
+//  Created by David Kochkin on 23.04.2024.
+//
+
+import UIKit
+
+
+struct RMSettingsViewVM {
+    let cellViewModels: [RMSettingsCellVM]
+ 
+    init() {
+        
+    }
+}
