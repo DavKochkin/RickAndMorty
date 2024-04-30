6c4bc64e7f7cab1db1251bf07267dce104388ba8
David Kochkin <kochkin.david@gmail.com>
2024-04-30 18:28:24 +0300

    Add RMLocationView

---
+++ RickAndMorty/Views/Location/RMLocationView.swift
@@ -0,0 +1,56 @@
+//
+//  RMLocationView.swift
+//  RickAndMorty
+//
+//  Created by David Kochkin on 30.04.2024.
+//
+
+import UIKit
+
+final class RMLocationView: UIView {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.alpha    = 0
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
+    //MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubviews(tableView, spinner)
+        spinner.startAnimating()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leftAnchor.constraint(equalTo: leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+}
