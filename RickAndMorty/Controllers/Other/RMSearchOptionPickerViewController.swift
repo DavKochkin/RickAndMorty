5be4f579864439d5b232e59f6a5256882b267cf4
David Kochkin <kochkin.david@gmail.com>
2024-05-14 14:42:03 +0300

    Setup TableView

--- RickAndMorty/Controllers/Other/RMSearchOptionPickerViewController.swift
+++ RickAndMorty/Controllers/Other/RMSearchOptionPickerViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 final class RMSearchOptionPickerViewController: UIViewController {
     private let option: RMSearchInputViewVM.DynamicOption
     
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: "cell"
+        )
+        return table
+    }()
+    
     // MARK: - Init
     
     init(option: RMSearchInputViewVM.DynamicOption) {
@@ -26,5 +36,36 @@ final class RMSearchOptionPickerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        setUpTable()
+    }
+    
+    private func setUpTable() {
+        view.addSubview(tableView)
+        tableView.delegate   = self
+        tableView.dataSource = self
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+}
+
+
+extension RMSearchOptionPickerViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello,world "
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
--- RickAndMorty/Controllers/Other/RMSearchViewController.swift
+++ RickAndMorty/Controllers/Other/RMSearchViewController.swift
@@ -90,7 +90,7 @@ class RMSearchViewController: UIViewController {
 
 extension RMSearchViewController: RMSearchViewDelegate {
     func rmSearchView(_ searchView: RMSearchView, didSelectOption option: RMSearchInputViewVM.DynamicOption) {
-        let vc = RMSearchOptionPickerViewController()
+        let vc = RMSearchOptionPickerViewController(option: option)
         vc.sheetPresentationController?.detents = [.medium()]
         vc.sheetPresentationController?.prefersGrabberVisible = true
         present(vc, animated: true)
